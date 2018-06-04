c58a56426c8dcc236a8437508f1aeba8fbb69e38
abassi_ <walid.abbassi@esprit.tn>
2018-06-04 08:34:28 +0200

    add enum
    add enum

---
+++ general coding/enum/enum/season.h
@@ -0,0 +1,2 @@
+typedef struct { int value; } season;
+extern const season spring, summer, autumn, winter;
---
+++ general coding/enum/enum/suit.h
@@ -0,0 +1,2 @@
+typedef struct { int value; } suit;
+extern const suit clubs, diamonds, hearts, spades;
